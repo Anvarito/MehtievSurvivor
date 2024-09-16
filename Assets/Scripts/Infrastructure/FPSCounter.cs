0f513bc73f71cdff114a3cc4035622e67e4093e2
Anvar <anvaritohaz@gmail.com>
2024-09-16 21:06:33 +0500

    microfix

--- Assets/Scripts/Infrastructure/FPSCounter.cs
+++ Assets/Scripts/Infrastructure/FPSCounter.cs
@@ -5,7 +5,6 @@ namespace Infrastructure
 {
     public class FPSCounter : MonoBehaviour
     {
-        public TextMeshProUGUI _label;
         private int frameCount = 0;
         private float elapsedTime = 0.0f;
         private float fps = 0.0f;
@@ -28,19 +27,18 @@ namespace Infrastructure
                 fps = frameCount / elapsedTime;
                 frameCount = 0;
                 elapsedTime = 0.0f;
-                _label.text = "FPS: " + Mathf.Ceil(fps);
             }
         }
 
         void OnGUI()
         {
-            return;
+            
             int width = 200;
             int height = 40;
         
             Rect rect = new Rect(Screen.width - width, 10, width, height);
         
-            GUI.Label(rect, "<size=20>" + " FPS" + Mathf.Ceil(fps) + "</size>");
+            GUI.Label(rect, "<size=20>" + " FPS " + Mathf.Ceil(fps) + "</size>");
         }
     }
 }
--- Assets/Scripts/Player/CameraMover.cs
+++ Assets/Scripts/Player/CameraMover.cs
@@ -4,9 +4,13 @@ public class CameraMover : MonoBehaviour
 {
     [SerializeField] private Transform _targetToMove;
     [SerializeField] private Vector3 _offset;
-   
+    private Vector3 _oldPosition;
+
     void Update()
     {
-        transform.position = _offset + _targetToMove.position;
+        if (_targetToMove.position != _oldPosition)
+            transform.position = _offset + _targetToMove.position;
+
+        _oldPosition = _targetToMove.position;
     }
 }
